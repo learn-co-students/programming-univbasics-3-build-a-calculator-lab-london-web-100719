68e1fa4d9601f61a50bb85c29cc4cc539eadc86b
jules blanc <jules.blanc@unccub.com>
2019-10-03 09:31:18 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,25 @@
 def addition(num1, num2)
-
+  add = num1 + num2
+  return add 
 end
 
 def subtraction(num1, num2)
-
+  subs = num1 - num2
+  return subs
 end
 
 def division(num1, num2)
-
+  return num1/num2
 end
 
 def multiplication(num1, num2)
-
+  return num1*num2
 end
 
 def modulo(num1, num2)
-
+  return num1%num2
 end
 
 def square_root(num)
-
+  Math.sqrt(num)
 end
